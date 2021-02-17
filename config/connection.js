78280c2e74cb9727c3077b7f02d03ad941d378b0
David Johnson <david.m.johnson32@gmail.com>
2021-02-16 22:12:33 -0800

    Updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,18 @@
 const mysql = require('mysql');
 const env = require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: process.env.db_password,
-  database: 'burgers_db',
-});
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+ connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    // NOTE: Be sure to add your MySQL password here!
+    password: process.env.db_password,
+    database: 'burgers_db',
+  });
+}
 // connection.connect((err) => {
 //   if (err) {
 //     console.error(`error connecting: ${err.stack}`);
